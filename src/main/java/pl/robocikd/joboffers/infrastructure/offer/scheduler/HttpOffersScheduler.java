22b8264ab9820af0439fb64f5d6e4e132883458c
Damian <robocikd@wp.pl>
2023-08-14 14:32:23 +0200

    add scheduler test, awaitility

---
+++ src/main/java/pl/robocikd/joboffers/infrastructure/offer/scheduler/HttpOffersScheduler.java
@@ -0,0 +1,33 @@
+package pl.robocikd.joboffers.infrastructure.offer.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pl.robocikd.joboffers.domain.offer.OfferFacade;
+import pl.robocikd.joboffers.domain.offer.dto.OfferResponseDto;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class HttpOffersScheduler {
+
+    private final OfferFacade offerFacade;
+    private static final String STARTED_OFFERS_FETCHING_MESSAGE = "Started offers fetching {}";
+    private static final String STOPPED_OFFERS_FETCHING_MESSAGE = "Stopped offers fetching {}";
+    private static final String ADDED_NEW_OFFERS_MESSAGE = "Added new {} offers";
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedDelayString = "${http.offers.scheduler.request.delay}")
+    public List<OfferResponseDto> fetchAllOffersAndSaveAllIfNotExists() {
+        log.info("Started offers fetching {}", dateFormat.format(new Date()));
+        final List<OfferResponseDto> addedOffers = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
+        log.info(ADDED_NEW_OFFERS_MESSAGE, addedOffers.size());
+        log.info(STOPPED_OFFERS_FETCHING_MESSAGE, dateFormat.format(new Date()));
+        return addedOffers;
+    }
+}
