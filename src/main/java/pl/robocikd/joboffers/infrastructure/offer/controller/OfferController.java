684e5b9157b84d8563b630ba948b3db6ce8922b4
Damian <robocikd@wp.pl>
2023-08-16 12:02:58 +0200

    implement rest controller to get offers, test
    (cherry picked from commit 6b209861152e1d92824d3de8de1242dd763ad80a)

---
+++ src/main/java/pl/robocikd/joboffers/infrastructure/offer/controller/OfferController.java
@@ -0,0 +1,23 @@
+package pl.robocikd.joboffers.infrastructure.offer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.robocikd.joboffers.domain.offer.OfferFacade;
+import pl.robocikd.joboffers.domain.offer.dto.OfferResponseDto;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class OfferController {
+
+    private final OfferFacade offerFacade;
+
+    @GetMapping("/offers")
+    public ResponseEntity<List<OfferResponseDto>> findAllOffers() {
+        List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
+        return ResponseEntity.ok(allOffers);
+    }
+}
