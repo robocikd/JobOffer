4028478604b883e2a6521744940740ee9a8e2d3b
Damian <robocikd@wp.pl>
2023-08-17 12:53:33 +0200

    add findOfferById and exception handler

--- src/main/java/pl/robocikd/joboffers/infrastructure/offer/controller/OfferController.java
+++ src/main/java/pl/robocikd/joboffers/infrastructure/offer/controller/OfferController.java
@@ -3,6 +3,7 @@ package pl.robocikd.joboffers.infrastructure.offer.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.robocikd.joboffers.domain.offer.OfferFacade;
 import pl.robocikd.joboffers.domain.offer.dto.OfferResponseDto;
@@ -19,5 +20,11 @@ public class OfferController {
     public ResponseEntity<List<OfferResponseDto>> findAllOffers() {
         List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
         return ResponseEntity.ok(allOffers);
+    }
+
+    @GetMapping("/offers/{id}")
+    public ResponseEntity<OfferResponseDto> findOfferById(@PathVariable String id) {
+        OfferResponseDto offerById = offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offerById);
     }
 }
