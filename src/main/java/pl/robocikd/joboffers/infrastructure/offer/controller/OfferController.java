5d776a61abb0e9bf1938b1fe5965f03f2814155a
Damian <robocikd@wp.pl>
2023-08-18 16:20:26 +0200

    offer validation on saveOffer, offer duplicateKeyException, tests

--- src/main/java/pl/robocikd/joboffers/infrastructure/offer/controller/OfferController.java
+++ src/main/java/pl/robocikd/joboffers/infrastructure/offer/controller/OfferController.java
@@ -1,13 +1,18 @@
 package pl.robocikd.joboffers.infrastructure.offer.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.robocikd.joboffers.domain.offer.OfferFacade;
+import pl.robocikd.joboffers.domain.offer.dto.OfferRequestDto;
 import pl.robocikd.joboffers.domain.offer.dto.OfferResponseDto;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -26,5 +31,11 @@ public class OfferController {
     public ResponseEntity<OfferResponseDto> findOfferById(@PathVariable String id) {
         OfferResponseDto offerById = offerFacade.findOfferById(id);
         return ResponseEntity.ok(offerById);
+    }
+
+    @PostMapping("/offers")
+    public ResponseEntity<OfferResponseDto> saveOffer(@RequestBody @Valid OfferRequestDto offerRequestDto) {
+        OfferResponseDto offerResponseDto = offerFacade.saveOffer(offerRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(offerResponseDto);
     }
 }
